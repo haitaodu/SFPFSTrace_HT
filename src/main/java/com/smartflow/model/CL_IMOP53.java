2aa3b3fc410407a8e272a84588f2f32cd3999548
haitaodu <1009779981@qq.com>
2020-09-24 14:13:40 +0800

    imop53 无条形码bug修改

--- src/main/java/com/smartflow/model/CL_IMOP53.java
+++ src/main/java/com/smartflow/model/CL_IMOP53.java
@@ -24,6 +24,16 @@ public class CL_IMOP53 {
     private String DB2000_DBD44;
     private String DB2000_DBD48;
     private String IS_OK;
+    private String SerialNumber;
+
+
+    public String getSerialNumber() {
+        return SerialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        SerialNumber = serialNumber;
+    }
 
     public String getUUID() {
         return UUID;
