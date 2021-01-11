1828cfc96a7d265e2f58cb8d008617d8fd47bfe2
wangjunjia955 <wjj2239387608@qq.com>
2021-01-11 18:11:42 +0800

    add dto CellSerialNumberDTO

---
+++ src/main/java/com/smartflow/dto/CellSerialNumberDTO.java
@@ -0,0 +1,49 @@
+package com.smartflow.dto;
+
+/**
+ * @Author: WJJ
+ * @Description:
+ * @Date: 2021-01-08 14:10
+ **/
+public class CellSerialNumberDTO {
+    private String TUSerialNumber;
+    private String IMSerialNumber;
+    private String RESerialNumber;
+    private String PDSerialNumber;
+    private String FCSerialNumber;
+    public String getTUSerialNumber() {
+        return TUSerialNumber;
+    }
+
+    public void setTUSerialNumber(String TUSerialNumber) {
+        this.TUSerialNumber = TUSerialNumber;
+    }
+    public String getIMSerialNumber() {
+        return IMSerialNumber;
+    }
+
+    public void setIMSerialNumber(String IMSerialNumber) {
+        this.IMSerialNumber = IMSerialNumber;
+    }
+    public String getRESerialNumber() {
+        return RESerialNumber;
+    }
+
+    public void setRESerialNumber(String RESerialNumber) {
+        this.RESerialNumber = RESerialNumber;
+    }
+    public String getPDSerialNumber() {
+        return PDSerialNumber;
+    }
+
+    public void setPDSerialNumber(String PDSerialNumber) {
+        this.PDSerialNumber = PDSerialNumber;
+    }
+    public String getFCSerialNumber() {
+        return FCSerialNumber;
+    }
+
+    public void setFCSerialNumber(String FCSerialNumber) {
+        this.FCSerialNumber = FCSerialNumber;
+    }
+}
